aa7cdfad88104801c75488bd98fb4e9769d6e1be
Alexis Moreau <alexismoreau2@live.fr>
2016-11-23 21:32:10 +0100

    initial commit + configure xo

---
+++ README.md
@@ -0,0 +1,14 @@
+This is a meteor/ node based project 
+
+# Requirement:
+- npm
+- meteor
+
+# Install
+```
+npm install; meteor;
+```
+
+# Enjoy
+browse to http://localhost:3000
+
---
+++ server/main.js
@@ -0,0 +1,21 @@
+import {Meteor} from 'meteor/meteor';
+
+const distance = require('google-distance');
+
+Meteor.startup(() => {
+  // code to run on server at startup
+
+});
+
+distance.get(
+  {
+    origin: 'San Francisco, CA',
+    destination: 'San Diego, CA'
+  },
+  (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+  }
+);
