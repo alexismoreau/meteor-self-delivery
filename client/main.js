f6d9a825118eab9bf5e37f7c9c0f2848df76c2f4
Alexis Moreau <alexismoreau2@live.fr>
2016-11-23 22:01:17 +0100

    updated xo exceptions

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
-import {Template} from 'meteor/templating';
-import {ReactiveVar} from 'meteor/reactive-var';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
